3a2069cbdd5f126abbdd7abc41a46c0dfda979e7
methodmen <meth>
2018-07-09 10:00:42 +0900

    fixed pgup

--- keyhac/config.py
+++ keyhac/config.py
@@ -105,7 +105,7 @@ def configure(keymap):
             keymap_global[k +
                           "U1-S-6"] = k + "Home"  # Move to beginning of line
             keymap_global[k + "U1-S-4"] = k + "End"  # Move to end of line
-            keymap_global[k + "U1-Period"] = k + "PageUp"  # Page up
+            keymap_global[k + "U1-Comma"] = k + "PageUp"  # Page up
             keymap_global[k + "U1-m"] = k + "PageDown"  # Page down
             keymap_global[k + "U1-x"] = k + "Delete"  # Delete
             keymap_global[k + "U1-n"] = k + "Back"  # Back space
